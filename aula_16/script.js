c0225fa95357a60c980cf4aa6f2fe82649a04a5d
NCastedo <castedonicoly15@gmail.com>
2024-08-05 10:37:43 -0300

    Add files via upload

---
+++ aula_16/script.js
@@ -0,0 +1,22 @@
+const RESULTADO = document.querySelector('.result');
+
+function inserir(valor){
+    RESULTADO.innerHTML += valor;
+}
+
+function limpar(){
+    RESULTADO.innerHTML = '';
+}
+
+function apagar(){
+    if(RESULTADO.textContent){
+        let remove = document.querySelector('.result').innerHTML;
+        RESULTADO.innerHTML = remove.substring(0, remove.length - 1);
+    }
+}
+
+function confirmar(){
+    if (RESULTADO.textContext != 'Erro'){
+        document.getElementById('resultado').innerHTML - eval(RESULTADO.innerHTML);
+    }
+}
